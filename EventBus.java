3bfe18a373b3180ec0c7fd30bc131507ce8ebe9a
Diego Castaneda <diego.castaneda35@uabc.edu.mx>
2025-02-24 13:57:45 -0800

    Arquitectura base

---
+++ EventBus.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * EventBus
+ */
+public class EventBus {
+    private static volatile ArrayList<Event> events = new ArrayList<Event>(); 
+    private static HashMap<Class<? extends Event>, ArrayList<Consumer>> subscriptions = new HashMap<Class<? extends Event>,ArrayList<Consumer>>();
+
+    public static void publish(Event e){
+        events.add(e);
+        System.out.println(events.size());
+    }
+
+    public static boolean subscribe(Consumer c, Class<? extends Event> e){
+        if(subscriptions.get(e) == null)
+            subscriptions.put(e, new ArrayList<Consumer>());
+
+        if(subscriptions.get(e).contains(c))
+            return false;
+
+        subscriptions.get(e).add(c);
+        System.out.println("Subscribed consumer: " + c + " to event: " + e.getName());
+        return true;
+    }
+
+    public static void dispatch(){
+        while (events.iterator().hasNext()) {
+            Event event = events.iterator().next();
+            for (Consumer c: subscriptions.get(event.getClass())) {
+                c.handle(event); 
+            }
+            events.remove(event);
+        }
+    }
+}
